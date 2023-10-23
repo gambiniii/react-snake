bf9c7558dca94e6dc2267a0f264cd9f2bc6aeb52
gambiniii <lucasgambini3@gmail.com>
2023-10-23 18:46:57 -0300

    Implementada a funcionalidade de movimento automático da cobra, igual ao jogo original. Refatorei uma parte do código também

--- src/pages/Screen/screen.js
+++ src/pages/Screen/screen.js
@@ -1,103 +1,134 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Frame, Cell } from "./styled";
 
 export default function Screen() {
-    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const dimension = 24
+    const gameConfig = {
+        dimension: 24,
+        speed: 100,
+        initialDirection: 'Right',
+        coordinatesPattern: {
+            x: null,
+            y: null
+        },
+        initialBody: [
+            { x: 1, y: 0 },
+            { x: 1, y: 1 },
+            { x: 1, y: 2 },
+            { x: 1, y: 3 },
+        ]
+    }
 
-    const lines = []
-    const columns = []
     const frames = []
 
-    let initialBody = [
-        { x: 1, y: 0 },
-        { x: 1, y: 1 },
-        { x: 1, y: 2 },
-        { x: 1, y: 3 },
-    ]
-
-    const [headPosition, setHeadPosition] = useState({ x: 1, y: 3 });
-    const [body, setBody] = useState(initialBody)
-    const [fruit, setFruit] = useState({ x: 6, y: 8 })
+    const [headPosition, setHeadPosition] = useState(gameConfig.initialBody[gameConfig.initialBody.length - 1])
+    const [body, setBody] = useState(gameConfig.initialBody)
+    const [fruit, setFruit] = useState(gameConfig.coordinatesPattern)
+    const [direction, setDirection] = useState(gameConfig.initialDirection)
+    const [controls, setControls] = useState(true)
 
 
     function createFrames(dimension) {
-        for (let i = 0; i < dimension; i++) {
-            lines[i] = i + 1;
-            columns[i] = alphabet.charAt(i)
-        }
+        let lineBlock
 
-        lines.map(
-            (line) => {
-                let lineBlock = []
+        for (let line = 0; line < dimension; line++) {
+            lineBlock = []
 
-                columns.map(
-                    (column) => {
-                        lineBlock.push(
-                            {
-                                id: column + line,
-                                line: line,
-                                column: column
-                            }
-                        )
+            for (let column = 0; column < dimension; column++) {
+                lineBlock.push(
+                    {
+                        id: column + line,
+                        line: line,
+                        column: column
                     }
                 )
-                frames.push(lineBlock)
             }
-        )
+
+            frames.push(lineBlock)
+        }
     }
+
 
     function spawnFruit() {
-        let coordinates = {}
+        try {
+            let coordinates = {}
 
-        do {
-            coordinates = {
-                x: Math.floor(dimension - Math.random() * dimension),
-                y: Math.floor(dimension - Math.random() * dimension)
-            }
+            do {
+                coordinates = {
+                    x: Math.floor(gameConfig.dimension - Math.random() * gameConfig.dimension),
+                    y: Math.floor(gameConfig.dimension - Math.random() * gameConfig.dimension)
+                }
 
-        } while (body.some(segment => segment.x == coordinates.x && segment.y == coordinates.y))
+            } while (body.some(segment => segment.x === coordinates.x && segment.y === coordinates.y))
 
-        setFruit(coordinates)
-        console.log(coordinates)
+            setFruit(coordinates)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
-    function moveSnake(direction) {
+
+    function moveSnake(movement) {
         setHeadPosition((prevPosition) => {
             let newBody = {
-                x: prevPosition.x + direction.x,
-                y: prevPosition.y + direction.y
+                x: prevPosition.x + movement.x,
+                y: prevPosition.y + movement.y
             }
 
             setBody(body => {
                 body.push(newBody)
-
                 body.shift()
                 return body
             })
 
             colision(newBody)
+            setControls(true)
 
             return newBody
-        });
+        })
+
     }
+
 
     function command(event) {
         let { key } = event
 
-        if (key != 'F5') event.preventDefault()
+        if (key !== 'F5') event.preventDefault()
 
-        switch (key) {
-            case "ArrowUp":
+        setDirection(prevDirection => {
+            if (!controls) return prevDirection
+
+            switch (key) {
+                case "ArrowUp":
+                    if (prevDirection === 'Down') return prevDirection
+                    return 'Up'
+                case "ArrowDown":
+                    if (prevDirection === 'Up') return prevDirection
+                    return 'Down'
+                case "ArrowLeft":
+                    if (prevDirection === 'Right') return prevDirection
+                    return 'Left'
+                case "ArrowRight":
+                    if (prevDirection === 'Left') return prevDirection
+                    return 'Right'
+                default:
+                    return prevDirection
+            }
+        })
+    }
+
+
+    function movement() {
+        switch (direction) {
+            case "Up":
                 moveSnake({ x: -1, y: 0 });
                 break;
-            case "ArrowDown":
+            case "Down":
                 moveSnake({ x: 1, y: 0 });
                 break;
-            case "ArrowLeft":
+            case "Left":
                 moveSnake({ x: 0, y: -1 });
                 break;
-            case "ArrowRight":
+            case "Right":
                 moveSnake({ x: 0, y: 1 });
                 break;
             default:
@@ -105,89 +136,113 @@ export default function Screen() {
         }
     }
 
+    function tick() {
+        window.clearTimeout()
+        setTimeout(() => {
+            movement()
+        }, gameConfig.speed)
+    }
+
+
     function colision(coordinates) {
         let corpo = body.slice(0, body.length - 1)
 
-        console.log(corpo)
-        console.log(body)
-        console.log(headPosition)
-
         for (let segment in corpo) {
-            console.log()
-
-            if (corpo[segment].x == coordinates.x && corpo[segment].y == coordinates.y) alert("P E R D E U")
+            if (corpo[segment].x === coordinates.x && corpo[segment].y === coordinates.y) alert("P E R D E U")
         }
     }
+
 
     function classifier(frame) {
         let className = ""
 
-        if (fruit.x == frame.line - 1 && fruit.y == alphabet.indexOf(frame.column)) className = 'fruit'
-        if (body.find(segment => segment.x == frame.line - 1 && segment.y == alphabet.indexOf(frame.column))) className += ' body'
-        if (body[body.length - 1].x == frame.line - 1 && body[body.length - 1].y == alphabet.indexOf(frame.column)) className += ' head'
+        if (fruit.x === frame.line && fruit.y === frame.column) className = 'fruit'
+        if (body.find(segment => segment.x === frame.line && segment.y === frame.column)) className += ' body'
+        if (body[body.length - 1].x === frame.line && body[body.length - 1].y === frame.column) className += ' head'
 
         return className
     }
 
-    createFrames(dimension)
+
+    createFrames(gameConfig.dimension)
 
     useEffect(() => {
         document.addEventListener('keydown', command);
         spawnFruit()
+        movement()
         return () => {
             document.removeEventListener('keydown', command);
         };
     }, [])
+
 
     useEffect(() => {
         if (headPosition.x === fruit.x && headPosition.y === fruit.y) {
             let newBody = body
             newBody.unshift({ ...fruit })
             setBody(newBody)
-            console.log(body)
             spawnFruit()
         }
 
+        tick()
     }, [headPosition])
 
-    return (
-        <div>
-            <h1>Tela</h1>
+    useEffect(() => {
+        setControls(false)
+    }, [direction])
 
-            <Frame>
-                {
-                    frames.map((line) => {
-                        return (
-                            <div style={{ display: "flex" }}>
-                                {
-                                    line.map(({ id, line, column }) => {
-                                        return (
-                                            <Cell
-                                                id={id}
-                                                className={
-                                                    classifier({ id, line, column })
-                                                }
-                                            />
-                                        )
-                                    })
-                                }
-                            </div>
-                        )
-                    })
-                }
-            </Frame>
-            <div style={{ fontSize: "24px", fontWeight: "bold" }}>
-                <p>Olá <span style={
+
+    return (
+        <div style={{ display: 'flex' }}>
+
+            <div>
+                <h1>Tela</h1>
+
+                <Frame>
                     {
-                        backgroundImage: "linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red)",
-                        color: "transparent",
-                        WebkitBackgroundClip: "text"
-                    }}>
-                    @Recruiter
-                </span>! </p>
-                <p>Esse projeto ainda não está finalizado</p>
-                <p>Bjs!</p>
+                        frames.map((frame) => {
+                            return (
+                                <div style={{ display: "flex" }}>
+                                    {
+                                        frame.map(({ id, line, column }) => {
+                                            return (
+                                                <Cell
+                                                    id={id}
+                                                    className={
+                                                        classifier({ line, column })
+                                                    }
+                                                />
+                                            )
+                                        })
+                                    }
+                                </div>
+                            )
+                        })
+                    }
+                </Frame>
+
+                <div style={{ fontSize: "24px", fontWeight: "bold" }}>
+                    <p>Olá
+                        <span style={
+                            {
+                                backgroundImage: "linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red)",
+                                color: "transparent",
+                                WebkitBackgroundClip: "text"
+                            }}>
+                            @Recruiter
+                        </span>!
+                    </p>
+                    <p>ESTE PROJETO AINDA ESTÁ SENDO DESENVOLVIDO!</p>
+                    <p>Bjs!</p>
+                </div>
+            </div >
+
+            <div>
+                <h3>Direção: {direction}</h3>
+                <h3>Controles: {controls ? 'enabled' : 'disabled'}</h3>
             </div>
-        </div >
+
+        </div>
+
     )
 }
